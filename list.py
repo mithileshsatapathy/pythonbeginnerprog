21832f5db9e7243d3a181097ceaa08e09f4308ab
Mithilesh Satapathy <mithileshsatapathy@outlook.com>
2020-04-25 13:04:21 -0500

    Add files via upload

---
+++ list.py
@@ -0,0 +1,43 @@
+
+###1.0---- This is a nested list to Print---###
+
+##Numbers = [1,2,3,[4,5,6,[7,8,9]]]
+##
+##for each_num in Numbers:
+##        if isinstance (each_num,list):
+##                for sub_num in each_num:
+##                        if isinstance (sub_num,list):
+##                                for nest_num in sub_num:
+##                                        print (nest_num)
+##                        else:
+##                                print (sub_num)
+##        else:
+##                print(each_num)
+##
+##
+
+###2.0--- This is to print odd and even number ---###
+##array = [23,45,67,89,56,46,37,78]
+##for each_num in array:
+##        if each_num % 2 ==0:
+##                print("This is an even number")
+##        else:
+##                print ("This is an odd number")
+
+###3.0--- Print name and age as user input ---###
+
+##Name = input("Enter your Name:")
+##print ("Your Name is : " + Name)
+##Age= int(input("Enter your Age:"))
+##print("Your Age is: " + str(Age))
+##turn100Years= str(2020-Age+100)
+##print("You will turn 100 Years old in the Year :" + str(turn100Years))
+
+###---List Less Than Ten---###
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = []
+for i in a:
+        if i<10:
+                b.append(i)
+print(b)
